6d5ed15fcd3375ad4e557c601e4022c85fe1d459
AbhishekkGautam <abhishekgautm243@gmail.com>
2021-07-26 00:39:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="hero">
+        <h1 className="hero-heading">Speak Like Yoda</h1>
       </header>
+      <section className="section">
+        <div className="container container-center section-center">
+          <input placeholder="Enter emoji or click on any emoji below" />
+          <input placeholder="Enter emoji or click on any emoji below" />
+          <button id="btn-translate">Translate</button>
+          <div className="outputDiv">
+            <p>Yoda's way of speaking</p>
+
+            <div id="output-div"></div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
